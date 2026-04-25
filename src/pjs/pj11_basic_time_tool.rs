cf62de29ad3d2289336257bc55bc22c2372da45b
nazguul <youneshabbal1@gmail.com>
2026-04-25 21:55:02 +0100

    pj11_basic_time_tool.rs, added input logic

--- src/pjs/pj11_basic_time_tool.rs
+++ src/pjs/pj11_basic_time_tool.rs
@@ -4,30 +4,28 @@
 use std::io::{Write, stdin, stdout};
 
 pub fn timer_tool() {
-    println!("{:?}",get_time_input())
+    println!("{:?}", get_time_input())
 }
 fn get_time_input() -> Option<(u64, u64, u64)> {
-        println!("welcome to timer ");
-        print!("enter the time (e,g,. HH:MM:SS): ");
-        let _ = stdout().flush();
-        let mut input = String::new();
-        stdin()
-            .read_line(&mut input)
-            .expect("ERROR: failed to read input text");
-        // to lowercase created new temp string , in the end the string is dropped , the vectore became pointing to nothing (dangling ptr)
-        let vector: Vec<&str> = input.trim().split(':').collect();
-        if &vector.len() != &3 {
-            return None
-        }
+    println!("welcome to timer ");
+    print!("enter the time (e,g,. HH:MM:SS): ");
+    let _ = stdout().flush();
+    let mut input = String::new();
+    stdin()
+        .read_line(&mut input)
+        .expect("ERROR: failed to read input text");
+    // to lowercase created new temp string , in the end the string is dropped , the vectore became pointing to nothing (dangling ptr)
+    let vector: Vec<&str> = input.trim().split(':').collect();
+    if &vector.len() != &3 {
+        return None;
+    }
 
-        let hours :u64= vector[0].parse().ok()?;
-        let minutes :u64= vector[1].parse().ok()?;
-        let seconds :u64= vector[2].parse().ok()?;
-    Some((hours,minutes,seconds))
-
+    let hours: u64 = vector[0].parse().ok()?;
+    let minutes: u64 = vector[1].parse().ok()?;
+    let seconds: u64 = vector[2].parse().ok()?;
+    Some((hours, minutes, seconds))
 }
 #[test]
-fn test () {
-
+fn test() {
     timer_tool()
 }

b819bc1a7f6b8f23c2c213c92dc73661d4045ed6
nazguul <youneshabbal1@gmail.com>
2026-04-26 14:12:40 +0100

    pj11_basic_time_tool.rs, implemented the mpsc logic and successfully works , but still cpu usage is high

--- src/pjs/pj11_basic_time_tool.rs
+++ src/pjs/pj11_basic_time_tool.rs
@@ -1,49 +1,76 @@
-// objective:
-// like the timer in the mobile , you enter the time and after that time passed reminds you with some kind of action
-
 use std::io::{Write, stdin, stdout};
+use std::sync::mpsc::channel;
 use std::thread;
 use std::time::Duration;
 
 pub fn timer_tool() {
-
-    timer(&get_time_input())
-}
-fn get_time_input() -> Vec<u64> {
-    println!("welcome to timer ");
-    let mut vector = Vec::with_capacity(3);
+    let mut vector: Vec<u64> = Vec::with_capacity(3);
+    println!("welcome to basic timer");
     loop {
-        print!("enter the time (e,g,. HH:MM:SS): ");
+        print!("enter time (e.g. hh:mm:ss): ");
         let _ = stdout().flush();
         let mut input = String::new();
         stdin()
             .read_line(&mut input)
-            .expect("ERROR: failed to read input text");
-        // to lowercase created new temp string , in the end the string is dropped , the vector became pointing to nothing (dangling ptr)
-        vector = input.trim().split(':').filter_map(|str| str.parse::<u64>().ok()).collect();
-        if vector.len() != 3 {
-            println!("enter a valid time");
-            continue;
-        }
-        break;
-    }
-    vector
-}
-fn timer(time: &Vec<u64>) {
-    let mut total_time = time[0] * 3600 + time[1] * 60 + time[2];
-    while total_time > 0 {
-        let hours = total_time / 3600;
-        let minutes = (total_time/3600) % 60 ;
-        let seconds = total_time % 60;
+            .expect("failed to read the input ");
+        vector = input
+            .trim()
+            .split(":")
+            .filter_map(|time| match time.parse::<u64>() {
+                Ok(val) => Some(val),
+                Err(_) => {
+                    println!("'{}' is not a valid number, skipping...", time);
+                    None
+                }
+            })
+            .collect();
 
-        // \r for making the cursor of writing pointing to same place , so we overwrite previous time
-        print!("\r{:02}:{:02}:{:02}",hours,minutes,seconds);
-        let _ =  stdout().flush();
-        thread::sleep(Duration::from_secs(1));
-        total_time-=1;
+        if vector.len() == 3 { break } else { continue }
     }
-}
-#[test]
-fn test() {
-    timer_tool()
+    let mut total_seconds = vector[0] * 3600 + vector[1] * 60 + vector[2];
+    println!("press 'p' for pause or press 'r' for resume");
+    let (tx, rx) = channel();
+    thread::spawn(move || {
+        loop {
+            let mut input = String::new();
+            stdin().read_line(&mut input).unwrap();
+            match input.trim().to_lowercase().as_str() {
+                "p" => {
+                    println!("paused");
+                    tx.send(true).unwrap()
+                }
+                "r" => {
+                    println!("resumed");
+                    tx.send(false).unwrap()
+                }
+                _ => {
+                    println!("invalid key");
+                    continue;
+                }
+            }
+        }
+    });
+    let mut paused = false; // Start NOT paused (timer running)
+
+    // main thread
+    while total_seconds > 0 {
+        let hours = total_seconds / 3600;
+        let minutes = (total_seconds % 3600) / 60;
+        let seconds = total_seconds % 60;
+        print!(
+            "\rremaining time is {:02} hours , {:02} minutes and {:02} seconds",
+            hours, minutes, seconds
+        );
+        let _ = stdout().flush();
+
+        if let Ok(new_paused_state) = rx.try_recv() {
+            paused = new_paused_state;
+        }
+
+        if !paused {
+            thread::sleep(Duration::from_secs(1));
+            total_seconds -= 1;
+        }
+    }
+    println!("\n⏰ Timer finished!");
 }

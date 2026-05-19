0728a119a193db3a7bf677fd9b3ddf67028c6087
nazguul <youneshabbal1@gmail.com>
2026-05-19 11:15:32 +0100

    pj30_producer_consumer_demo.rs, ez

--- src/pjs/pj30_producer_consumer_demo.rs
+++ src/pjs/pj30_producer_consumer_demo.rs
@@ -1,33 +1,33 @@
-use std::sync::mpsc::{self, SyncSender};
+// classic problem where producers producer values into a buffer and consumer consumes it
+// the problem is control the producers and consumers so we want resulted by buffer overflow or empty buffer
+
 use std::thread;
 use std::time::Duration;
 
-
-// demo from main repo 
 pub fn producer_consumer() {
-    println!("🔄 Producer-Consumer Demo");
+    let buffer_size = 3;
+    let (tx, rx) = std::sync::mpsc::sync_channel(buffer_size);
 
-    let (tx, rx) = mpsc::sync_channel(3); // bounded buffer size = 3
-
-    // Producer thread
+    // producer thread
     let producer = thread::spawn(move || {
-        for i in 1..=10 {
-            println!("📤 Producing task {}", i);
+        println!("producing values");
+        
+        for i in 0..10 {
             tx.send(i).unwrap();
-            thread::sleep(Duration::from_millis(300)); // simulate work
+            thread::sleep(Duration::from_millis(300))
         }
-        println!("✅ Producer finished.");
     });
 
-    // Consumer thread
+    // consumer thread
     let consumer = thread::spawn(move || {
         while let Ok(task) = rx.recv() {
-            println!("📥 Consuming task {}", task);
-            thread::sleep(Duration::from_millis(500)); // simulate slower work
+            println!("received value {task}");
+            thread::sleep(Duration::from_millis(300))
         }
     });
 
+    consumer.join().unwrap();
     producer.join().unwrap();
-    thread::sleep(Duration::from_secs(2)); // allow consumer to finish
-    println!("👋 All tasks processed.");
+    thread::sleep(Duration::from_secs(5));
+    println!("all tasks finished")
 }

225c6adc64764599fb176c0d7896d326e3e9f486
nazguul <youneshabbal1@gmail.com>
2026-06-02 14:06:33 +0100

    pj51_rest_api_server.rs, need more practice

--- src/pjs/pj51_rest_api_server.rs
+++ src/pjs/pj51_rest_api_server.rs
@@ -1,42 +1,56 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{App, HttpResponse, HttpServer, Responder, Result, get, post, web};
 use serde::{Deserialize, Serialize};
+use serde_json::json;
+use std::io;
 use std::sync::Mutex;
- 
-#[derive(Serialize, Deserialize, Clone)]
-struct Book {
-    id: usize,
-    title: String,
-    author: String,
-}
- 
+
+#[derive(Serialize, Deserialize)]
 struct AppState {
-    books: Mutex<Vec<Book>>,
+    books: Mutex<Vec<String>>,
 }
- 
-async fn get_books(data: web::Data<AppState>) -> impl Responder {
-    let books = data.books.lock().unwrap();
-    HttpResponse::Ok().json(&*books)
+#[derive(Serialize, Deserialize)]
+struct BookRequest {
+    book_name: String,
 }
- 
-async fn add_book(book: web::Json<Book>, data: web::Data<AppState>) -> impl Responder {
-    let mut books = data.books.lock().unwrap();
-    books.push(book.into_inner());
-    HttpResponse::Created().body("📚 Book added")
+#[get("/books")]
+async fn get_books(data: web::Data<AppState>) -> Result<impl Responder> {
+    let books_vec = data.books.lock().unwrap();
+    let html = format!(
+        "{}{}",
+        json!(&*books_vec),
+        r#"
+        <form action="/api/books" method="post">
+            <label for="name">add book:</label>
+            <input type="text" id="name" name="name" required>
+            <button type="submit">Add</button>
+        </form>
+        "#
+    );
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset: utf-8")
+        .body(html))
 }
- 
+#[post("/api/books")]
+async fn books_api(
+    query: web::Query<BookRequest>,
+    data: web::Data<AppState>,
+) -> Result<impl Responder> {
+    let mut books_vec = data.books.lock().unwrap();
+    books_vec.push(query.book_name.clone());
+    Ok(HttpResponse::Ok())
+}
+
 #[actix_web::main]
-pub async fn rest_api_server() -> std::io::Result<()> {
-    println!("📘 REST API running at http://127.0.0.1:8080");
- 
-    let books = web::Data::new(AppState {
-        books: Mutex::new(vec![]),
+pub async fn rest_api_server() -> io::Result<()> {
+    let books_vector = web::Data::new(AppState {
+        books: Mutex::new(vec!["book1".to_string(), "book2".to_string()]),
     });
- 
+    println!("server started at http://127.0.0.1:8080");
     HttpServer::new(move || {
         App::new()
-            .app_data(books.clone())
-            .route("/books", web::get().to(get_books))
-            .route("/books", web::post().to(add_book))
+            .app_data(books_vector.clone())
+            .service(get_books)
+            .service(books_api)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
